fc2bcd3d0845d901f4644d080cc6646de8fe6b8d
Nik-hub-dev <dasnikita217@gmail.com>
2026-05-10 13:14:36 +0530

    repo

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _net_project.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
